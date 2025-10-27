2ac35310152278ffba500cd40f1ee1a173c71358
Auspicious <uthmanabdulganiyu2019@gmail.com>
2025-10-27 12:55:02 +0100

    Improve AuthSuccessPage UI and functionality
    Enhance the authentication success page with a loading animation and message.

--- src/pages/auth/success.tsx
+++ src/pages/auth/success.tsx
@@ -1,14 +1,17 @@
 import { useRouter } from "next/router";
 import React, { useEffect } from "react";
+import { useAuth } from "@/context/AuthContext";
 
  const AuthSuccessPage = () => {
+  const { handleAuthentication } = useAuth();
   const router = useRouter();
   const searchParams = router.query;
   const token = searchParams.token as string;
 
   useEffect(() => {
     if (token) {
-      localStorage.setItem("access_token", token);
+      //localStorage.setItem("access_token", token);
+     handleAuthentication(token);
 
       router.push("/");
     } else {
@@ -16,7 +19,15 @@ import React, { useEffect } from "react";
     }
   }, [router]);
 
-  return <div>Authenticating...</div>;
+  return (
+   <div className="flex items-center justify-center min-h-screen">
+      <div className="text-center">
+        <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-blue-600 mx-auto mb-4"></div>
+        <h2 className="text-xl font-semibold mb-2">Authenticating...</h2>
+        <p className="text-gray-600">Please wait while we log you in.</p>
+      </div>
+    </div>
+  );
 };
 
 export default AuthSuccessPage
