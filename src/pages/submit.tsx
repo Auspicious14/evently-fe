ad8f1e92c6f8be2f4bfc08fa8eb81fd9558240be
Auspicious <uthmanabdulganiyu2019@gmail.com>
2025-10-29 10:46:01 +0100

    Refactor SubmitEventPage to remove SubmitProvider
    Removed SubmitProvider wrapper from SubmitEventPage.

--- src/pages/submit.tsx
+++ src/pages/submit.tsx
@@ -1,14 +1,8 @@
-import SubmitPage from "@/modules/submit/page";
-import { SubmitProvider } from "@/modules/submit/context";
+import { SubmitPage } from "@/modules/submit/page";
 
-// The event submission page.
-// We wrap it with the SubmitProvider to give the form and its children
-// access to the submission state and functions.
 const SubmitEventPage = () => {
   return (
-    <SubmitProvider>
       <SubmitPage />
-    </SubmitProvider>
   );
 };
 
