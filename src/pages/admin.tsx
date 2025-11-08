0d135e41dfdae7abf95007a5731e85e1ad544775
Auspicious <uthmanabdulganiyu2019@gmail.com>
2025-11-08 11:16:43 +0100

    Update admin.tsx

--- src/pages/admin.tsx
+++ src/pages/admin.tsx
@@ -1,13 +1,10 @@
 import { AdminDashboard } from "@/modules/admin/page"
 import AuthGuard from "@/components/AuthGuard"
-import { AdminProvider } from "@/modules/admin/context"
 
 const Admin = () => {
   return (
     <AuthGuard>
-      <AdminProvider>
         <AdminDashboard />
-      </AdminProvider>
     </AuthGuard>
   )
 }
