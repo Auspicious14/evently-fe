183ec24f982f47e3d601c9b0b5fa08188b06c49e
Auspicious <uthmanabdulganiyu2019@gmail.com>
2025-11-08 11:18:15 +0100

    Update dashboard.tsx

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,16 +1,10 @@
 import { DashboardPage } from "@/modules/dashboard/page"
 import AuthGuard from "@/components/AuthGuard"
-import { DashboardProvider } from "@/modules/dashboard/context"
-import { EventsProvider } from "@/modules/events/context"
 
 const Dashboard = () => {
   return (
     <AuthGuard>
-      <DashboardProvider>
-        <EventsProvider>
-          <DashboardPage />
-        </EventsProvider>
-      </DashboardProvider>
+     <DashboardPage />
     </AuthGuard>
   )
 }
