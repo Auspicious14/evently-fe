04f45fcd55033b90ecfb3c8c9bf86b4e5e704d76
Auspicious <uthmanabdulganiyu2019@gmail.com>
2025-11-08 11:18:55 +0100

    Update submit.tsx

--- src/pages/submit.tsx
+++ src/pages/submit.tsx
@@ -1,13 +1,10 @@
 import { SubmitPage } from "@/modules/submit/page";
 import AuthGuard from "@/components/AuthGuard";
-import { SubmitProvider } from "@/modules/submit/context";
 
 const SubmitEventPage = () => {
   return (
     <AuthGuard>
-      <SubmitProvider>
         <SubmitPage />
-      </SubmitProvider>
     </AuthGuard>
   );
 };
